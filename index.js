0e51c1f286d148bf11bf4bc0e77f2616cb1ae93c
Ham Choromi <choromi0804@gmail.com>
2019-06-21 02:58:41 +0900

    imported cookieParser and bodyParser

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -12,6 +14,9 @@ const handleListening = () =>
 const handleHome = (req, res) => res.send("Home");
 const handleProfile = (req, res) => res.send("Profile");
 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
