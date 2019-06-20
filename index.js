7e676dde825797984b3e781f90da7b1fff1fddfc
Ham Choromi <choromi0804@gmail.com>
2019-06-21 01:56:17 +0900

    Add Routes with express and using babel

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hi I'm Romi");
+import express from "express";
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () =>
+  console.log(`Listening on : http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Home");
+const handleProfile = (req, res) => res.send("Profile");
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
