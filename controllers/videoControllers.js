c08c7bfefd2ec75bf6a149898aac8f6d5f1348b5
Ham Choromi <choromi0804@gmail.com>
2019-06-25 01:07:42 +0900

    Set local variables for routes in pug

--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -1,7 +1,7 @@
 export const home = (req, res) => res.render("home");
 export const search = (req, res) => res.render("Search");
 export const video = (req, res) => res.render("Videos");
-export const upload = (req, res) => res.send("Uploads");
-export const videoDetail = (req, res) => res.send("Video Details");
-export const editVideo = (req, res) => res.send("Edit Video");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const upload = (req, res) => res.render("Uploads");
+export const videoDetail = (req, res) => res.render("Video Details");
+export const editVideo = (req, res) => res.render("Edit Video");
+export const deleteVideo = (req, res) => res.render("Delete Video");
