3f74e5286084f5d575997e9326e58f3609daf5b5
Ham Choromi <choromi0804@gmail.com>
2019-06-25 01:21:18 +0900

    Set template variables in pug

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,3 +1,4 @@
-export const join = (req, res) => res.render("Join");
-export const login = (req, res) => res.render("Login");
-export const logout = (req, res) => res.render("Logout");
+export const join = (req, res) => res.render("Join", { pageTitle: "Join" });
+export const login = (req, res) => res.render("Login", { pageTitle: "Login" });
+export const logout = (req, res) =>
+  res.render("Logout", { pageTitle: "Logout" });
--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -1,7 +1,13 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("Search");
-export const video = (req, res) => res.render("Videos");
-export const upload = (req, res) => res.render("Uploads");
-export const videoDetail = (req, res) => res.render("Video Details");
-export const editVideo = (req, res) => res.render("Edit Video");
-export const deleteVideo = (req, res) => res.render("Delete Video");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" });
+export const search = (req, res) =>
+  res.render("Search", { pageTitle: "Search" });
+export const video = (req, res) =>
+  res.render("Videos", { pageTitle: "Videos" });
+export const upload = (req, res) =>
+  res.render("Uploads", { pageTitle: "Uploads" });
+export const videoDetail = (req, res) =>
+  res.render("Video Details", { pageTitle: "Video Details" });
+export const editVideo = (req, res) =>
+  res.render("Edit Video", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) =>
+  res.render("Delete Video", { pageTitle: "Delete Video" });
