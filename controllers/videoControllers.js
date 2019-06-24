8dd10af14154a4f5d85dd6c55997dba96ef27119
Ham Choromi <choromi0804@gmail.com>
2019-06-24 23:30:21 +0900

    Revised Controller for rendering pug

--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -1,6 +1,6 @@
 export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.send("Search");
-export const video = (req, res) => res.send("Videos");
+export const search = (req, res) => res.render("Search");
+export const video = (req, res) => res.render("Videos");
 export const upload = (req, res) => res.send("Uploads");
 export const videoDetail = (req, res) => res.send("Video Details");
 export const editVideo = (req, res) => res.send("Edit Video");
