019faf73a85c49e4ddd3dea9045a9c5fee8a40c7
Ham Choromi <choromi0804@gmail.com>
2019-07-01 23:40:58 +0900

    created dummy data and rendered data title and details on home page

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+  {
+    id: 1004,
+    title: "Video Test 1",
+    description: "Video Test 1 test",
+    views: "24",
+    vidoeFile: "http://archive.org/details/BigBuckBunny_124",
+    creators: {
+      id: 1002,
+      name: "Romi",
+      email: "romi@romimail.com"
+    }
+  },
+  {
+    id: 1005,
+    title: "Video Test 2",
+    description: "Video Test 2 test",
+    views: "24",
+    vidoeFile: "http://archive.org/details/BigBuckBunny_124",
+    creators: {
+      id: 1002,
+      name: "Romi",
+      email: "romi@romimail.com"
+    }
+  },
+  {
+    id: 1006,
+    title: "Video Test 3",
+    description: "Video Test 3 test",
+    views: "24",
+    vidoeFile: "http://archive.org/details/BigBuckBunny_124",
+    creators: {
+      id: 1002,
+      name: "Romi",
+      email: "romi@romimail.com"
+    }
+  }
+];
