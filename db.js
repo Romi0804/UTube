d6ea3b252cf6488300ea02b644cc76db1b819615
Ham Choromi <choromi0804@gmail.com>
2019-07-11 01:41:34 +0900

    Installed mongoDB, mongoose and dotenv. Configured connection to mongoDB

--- db.js
+++ db.js
@@ -1,41 +1,14 @@
-export const videos = [
-  {
-    id: 1004,
-    title: "Video Test 1",
-    description: "Video Test 1 test",
-    views: "24",
-    vidoeFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creators: {
-      id: 1002,
-      name: "Romi",
-      email: "romi@romimail.com"
-    }
-  },
-  {
-    id: 1005,
-    title: "Video Test 2",
-    description: "Video Test 2 test",
-    views: "24",
-    vidoeFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creators: {
-      id: 1002,
-      name: "Romi",
-      email: "romi@romimail.com"
-    }
-  },
-  {
-    id: 1006,
-    title: "Video Test 3",
-    description: "Video Test 3 test",
-    views: "24",
-    vidoeFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creators: {
-      id: 1002,
-      name: "Romi",
-      email: "romi@romimail.com"
-    }
-  }
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/utube", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB");
+const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
