73acb339c874e3b92a64b75455772800a4b451de
Ham Choromi <choromi0804@gmail.com>
2019-07-14 02:18:03 +0900

    Built Upload and Create Video

--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -4,7 +4,6 @@ import Video from "../models/Video";
 export const home = async (req, res) => {
   try {
     const videos = await Video.find({});
-    //이렇게 하면 database의 모든 비디오를 가져온다.
     res.render("home", { pageTitle: "Home", videos });
   } catch (error) {
     console.log(error);
@@ -22,12 +21,18 @@ export const search = (req, res) => {
 export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const {
-    body: { file, title, description }
+    body: { title, description },
+    file: { path }
   } = req;
-  // To Do: Upload and save video
-  res.redirect(routes.videoDetail(324393));
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description
+  });
+  console.log(newVideo);
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = (req, res) =>
