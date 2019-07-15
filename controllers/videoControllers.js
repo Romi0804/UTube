7e4ae7d7297538e9aa67701035db8142e9352d5d
Ham Choromi <choromi0804@gmail.com>
2019-07-15 23:57:25 +0900

    Rendered video details when clicking on video

--- controllers/videoControllers.js
+++ controllers/videoControllers.js
@@ -35,8 +35,18 @@ export const postUpload = async (req, res) => {
   res.redirect(routes.videoDetail(newVideo.id));
 };
 
-export const videoDetail = (req, res) =>
-  res.render("videoDetail", { pageTitle: "Video Details" });
+export const videoDetail = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    res.render("videoDetail", { pageTitle: "Video Detail", video });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
+
 export const editVideo = (req, res) =>
   res.render("Edit Video", { pageTitle: "Edit Video" });
 export const deleteVideo = (req, res) =>
